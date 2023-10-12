e327289d6ddcd83bbbcb2ffaecea9d7aed82ce25
Ben Johnson <monk.sound.design@gmail.com>
2023-10-12 03:39:40 -0400

    navbar boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { Home, About, Store } from "./pages";
+import { Navbar } from "./components";
 
 function App() {
   return (
-    <Container>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
