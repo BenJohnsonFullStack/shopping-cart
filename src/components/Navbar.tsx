13b7e94cc62b93a6731ab51b033ff121826d897b
Ben Johnson <monk.sound.design@gmail.com>
2023-10-12 10:33:01 -0400

    navlinks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,26 @@
-import { Container, Navbar as NavbarBs } from "react-bootstrap";
+import { Container, Nav, Button, Navbar as NavbarBs } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { CartIcon } from "./svg";
 
 export function Navbar() {
   return (
     <NavbarBs className="bg-white shadow-sm mb-3">
-      <Container>Nav</Container>
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button>
+          <CartIcon />
+        </Button>
+      </Container>
     </NavbarBs>
   );
 }
