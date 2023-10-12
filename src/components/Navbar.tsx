6c5fbfde510626e84a20c9993e69a490370b6086
Ben Johnson <monk.sound.design@gmail.com>
2023-10-12 10:41:01 -0400

    cart icon and sticky nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { CartIcon } from "./svg";
 
 export function Navbar() {
   return (
-    <NavbarBs className="bg-white shadow-sm mb-3">
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
         <Nav className="me-auto">
           <Nav.Link to="/" as={NavLink}>
@@ -17,8 +17,24 @@ export function Navbar() {
             About
           </Nav.Link>
         </Nav>
-        <Button>
+        <Button
+          style={{ width: "3.5rem", height: "3.5rem", position: "relative" }}
+          variant="outline-none"
+        >
           <CartIcon />
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+            }}
+          >
+            3
+          </div>
         </Button>
       </Container>
     </NavbarBs>
