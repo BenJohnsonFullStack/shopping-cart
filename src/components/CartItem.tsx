2f676928c98494dfb43c998f4eb3007e6f7a8957
Ben Johnson <monk.sound.design@gmail.com>
2023-10-12 16:47:01 -0400

    cart total item price

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,6 +1,7 @@
-import { Image, Stack } from "react-bootstrap";
+import { Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context";
 import storeItems from "../data/items.json";
+import { formatCurrency } from "../utils";
 
 type CartItemProps = {
   id: number;
@@ -10,24 +11,28 @@ type CartItemProps = {
 export function CartItem({ id, quantity }: CartItemProps) {
   const { removeFromCart } = useShoppingCart();
   const item = storeItems.find((item) => item.id === id);
-  if (item === null) return null;
+  if (item == null) return null;
 
   return (
     <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
       <img
-        src={item?.imgUrl}
+        src={item.imgUrl}
         style={{ width: "125px", height: "75px", objectFit: "cover" }}
       />
       <div className="me-auto">
         <div>
-          {item?.name}{" "}
+          {item.name}{" "}
           {quantity > 1 && (
             <span className="text-muted" style={{ fontSize: "0.65rem" }}>
               x{quantity}
             </span>
           )}
         </div>
+        <div className="text-muted" style={{ fontSize: "0.75rem" }}>
+          {formatCurrency(item.price)}
+        </div>
       </div>
+      <div>{formatCurrency(item.price * quantity)}</div>
     </Stack>
   );
 }
